7b132f8e95c141c3eba347c3e4b39b937ed4dbf0
Emmanuel Medina <emmanuel.medina@univ-lorraine.fr>
2026-07-31 09:13:45 +0200

    Enhance LastnameDistribution with weights and constructor

--- src/distributions/LastnameDistribution.ts
+++ src/distributions/LastnameDistribution.ts
@@ -1,12 +1,20 @@
 import { Distribution } from "./Distribution.js";
 import { Person } from "../models/Person.js";
+import { Random } from "../stats/Random.js";
+import { LastnamePool } from "../stats/LastnamePool.js";
 
 export class LastnameDistribution
     implements Distribution<string>{
 
+    private values: string[];
+    private weights: number[];
+
+    constructor (lastnames: LastnamePool[]) {
+        this.values = lastnames.map((e) => e.name);
+        this.weights = lastnames.map((e) => e.count);
+    }
+
     sample(person:Partial<Person>):string{
-
-        return ""
-
+        return Random.weightedChoice(this.values, this.weights);
     }
 }
